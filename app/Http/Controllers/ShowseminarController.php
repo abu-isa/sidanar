fe41e05ff6208b078dcf78c75b5f3c27e627e394
abu-isa <toriabuisa@gmail.com>
2020-11-22 20:55:35 +0700

    test

--- app/Http/Controllers/ShowseminarController.php
+++ app/Http/Controllers/ShowseminarController.php
@@ -19,15 +19,15 @@ class ShowseminarController extends Controller
     	$currentDay = date('d');
     	$currentMonth = date('m');
 
-        $data = DB::table('seminar')->select('judul','tanggal','id')->whereDay('tanggal','>',$currentDay)->whereMonth('tanggal','=',$currentMonth)->where('status','1')->orderBy('tanggal', 'asc')->get();
+        $data = DB::table('seminars')->select('judul','tanggal','id')->whereDay('tanggal','>',$currentDay)->whereMonth('tanggal','=',$currentMonth)->where('status','1')->orderBy('tanggal', 'asc')->get();
         return view('daftarseminar',['data' => $data]);
     }
 
     public function showsuccess(Request $request, $id){
         DB::update('update form set status_pembayaran=? where id = ?',['success',$id]);
 
-        $form = DB::table('form')->where('id',$id)->get();
-        $data = DB::table('seminar')->where('id',$form[0]->pilihan)->get();
+        $form = DB::table('forms')->where('id',$id)->get();
+        $data = DB::table('seminars')->where('id',$form[0]->pilihan)->get();
         session()->flash('notifsuccess','Pembayaran telah diterima, terimakasih!');
         return view('ringkasanpembayaran',['data' => $data,'form'=>$form]);
     }
@@ -35,8 +35,8 @@ class ShowseminarController extends Controller
     public function showpending(Request $request, $id){
         DB::update('update form set status_pembayaran=? where id = ?',['pending',$id]);
 
-        $form = DB::table('form')->where('id',$id)->get();
-        $data = DB::table('seminar')->where('id',$form[0]->pilihan)->get();
+        $form = DB::table('forms')->where('id',$id)->get();
+        $data = DB::table('seminars')->where('id',$form[0]->pilihan)->get();
         session()->flash('notifpending','Pembayaran belum diterima, segera selesaikan pembayaran!');
         return view('ringkasanpembayaran',['data' => $data,'form'=>$form]);
     }
--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,11 @@ Route::get('/', function () {
 
 
 Route::get('/','ShowseminarController@index');
+Route::get('/daftar','ShowseminarController@show');
+Route::post('/create','AdddataController@insert');
+Route::get('/delete','DeleteseminarController@show');
+Route::get('hapus/{id}','DeleteseminarController@destroy');
+Route::post('addseminar','AddseminarController@addseminar');
+Route::get('homesuccess/{id}','ShowseminarController@showsuccess');
+Route::get('homepending/{id}','ShowseminarController@showpending');
+Route::get('/home','ShowseminarController@showhome');
