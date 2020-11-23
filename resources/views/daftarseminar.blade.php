84806a257924c903521413fa418549aeda458bca
abu-isa <toriabuisa@gmail.com>
2020-11-23 11:29:27 +0700

    Add daftarseminar in view

--- resources/views/daftarseminar.blade.php
+++ resources/views/daftarseminar.blade.php
@@ -7,7 +7,7 @@
 <title>Form</title>
 <link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" rel="stylesheet">
 <link rel="shortcut icon" href="{{asset('/frontend')}}/img/fav.png">
-<link rel="stylesheet" href="http://localhost/registform/public/css/fontstyle.css">
+<link rel="stylesheet" href="{{asset('css/fontstyle.css')}}">
 </head>
 <body>
 <div id="particles-js"></div>
