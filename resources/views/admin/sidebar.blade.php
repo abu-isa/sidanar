b29b52143aedab8e2dc09224c817178bb9d5ab42
abu-isa <toriabuisa@gmail.com>
2020-12-02 13:59:48 +0700

    V20.12.021400

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -5,7 +5,7 @@
         <!-- sidebar menu start-->
         <ul class="sidebar-menu">
           <li class="">
-            <a class="" href="index.html">
+            <a class="" href="{{ url('/admin') }}">
                           <i class="icon_house_alt"></i>
                           <span>Dashboard</span>
                       </a>
